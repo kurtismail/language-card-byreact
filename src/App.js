187628c7e75c606fa0040473bc24bbd4aaba8379
iso <ismailkurtlojistik@gmail.com>
2022-10-24 17:21:07 +0300

    commited

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Card from './components/Card';
+import {languages} from "./data";
+import Header from "./header/Header";
+
+
+function App() {
+  return (
+    <div className="main-container">
+    <Header/>
+    <Card/>
+    </div>
+  );
+}
+
+export default App;
